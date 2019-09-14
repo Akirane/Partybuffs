afea0320ecf380a92f55bba3574bd53f100f9bdc
Akirane <52178385+Akirane@users.noreply.github.com>
2019-09-14 15:22:45 +0200

    Update PartyBuffs.lua

--- PartyBuffs.lua
+++ PartyBuffs.lua
@@ -25,7 +25,7 @@ ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.]]
 
 _addon.name = 'PartyBuffs'
-_addon.author = 'Kenshi'
+_addon.author = 'Kenshi, Akirane'
 _addon.version = '3.0'
 _addon.commands = {'pb', 'partybuffs'}
 
